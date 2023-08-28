6525c0ea101bae246a6c5bb9c6d0e48e7ca314ed
Cythilya Tang <cythilya@gmail.com>
2023-08-28 17:24:52 +0800

    vs code settings

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log('test');
 Sentry.init({
   dsn: 'https://test12345.ingest.sentry.io/1qaz2wsx',
   integrations: [
